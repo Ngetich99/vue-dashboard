41efbde141741605d7caafc34185bf52d52c1c90
Ngetich99 <82973650+Ngetich99@users.noreply.github.com>
2025-12-05 23:39:27 -0800

    Update auth.js

--- src/stores/auth.js
+++ src/stores/auth.js
@@ -12,23 +12,16 @@ export const useAuthStore = defineStore('auth', {
   actions: {
     async login(email, password) {
       try {
-        const res = await axios.post(`${API}/auth/login`, {
-          email,
-          password
-        });
-
+        const res = await axios.post(`${API}/auth/login`, { email, password });
         this.token = res.data.token;
         this.user = res.data.user;
-
         localStorage.setItem("token", this.token);
-
         return res.data;
       } catch (error) {
         console.error(error);
         throw error;
       }
     },
-
     logout() {
       this.user = null;
       this.token = null;
