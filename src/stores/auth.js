6e8c62d509df40aecf25dc4a122a97a6e53fcd96
Ngetich99 <82973650+Ngetich99@users.noreply.github.com>
2025-12-05 23:16:01 -0800

    Create auth.js

---
+++ src/stores/auth.js
@@ -0,0 +1,38 @@
+import axios from 'axios';
+import { defineStore } from 'pinia';
+
+const API = import.meta.env.VITE_API_BASE_URL;
+
+export const useAuthStore = defineStore('auth', {
+  state: () => ({
+    user: null,
+    token: localStorage.getItem("token") || null,
+  }),
+
+  actions: {
+    async login(email, password) {
+      try {
+        const res = await axios.post(`${API}/auth/login`, {
+          email,
+          password
+        });
+
+        this.token = res.data.token;
+        this.user = res.data.user;
+
+        localStorage.setItem("token", this.token);
+
+        return res.data;
+      } catch (error) {
+        console.error(error);
+        throw error;
+      }
+    },
+
+    logout() {
+      this.user = null;
+      this.token = null;
+      localStorage.removeItem("token");
+    }
+  }
+});
