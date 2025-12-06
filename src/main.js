1435b0a2f37e2ea1f2d6fe12c7f5de3d66be7c34
Ngetich99 <82973650+Ngetich99@users.noreply.github.com>
2025-12-05 23:09:55 -0800

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,9 +1,11 @@
 import { createApp } from 'vue'
-import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
+import { createPinia } from 'pinia'
 
-const app = createApp(App)
-app.use(createPinia()) // <-- add this
-app.use(router)
-app.mount('#app')
+const pinia = createPinia()
+
+createApp(App)
+  .use(pinia)
+  .use(router)
+  .mount('#app')
